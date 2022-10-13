42cb97d4ccf425d09b3c56907d2063e9430e681b
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-10-13 11:41:30 -0300

    aula quinta

--- src/app/addchamado/addchamado.page.ts
+++ src/app/addchamado/addchamado.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../api/api-service';
 
 @Component({
   selector: 'app-addchamado',
@@ -8,7 +9,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddchamadoPage {
   formGroup: FormGroup;
-  constructor(public formBuilder: FormBuilder) {
+  isSubmitted: boolean = false;
+  foto_erro_chamado: string = '';
+
+  constructor(public formBuilder: FormBuilder, private apiService: ApiService) {
     this.formGroup = formBuilder.group({
       tituloChamado: [
         "",
@@ -27,13 +31,77 @@ export class AddchamadoPage {
           Validators.required
         ])
       ],
+
     });
   }
 
-  onSubmit(formData: any) {
-    console.log(formData);
+  get errorControl() {
+    return this.formGroup.controls;
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+
+    if(!this.formGroup.valid){
+      console.log('Por favor preencha todos os campos');
+      return false;
+    }
+    //verificando se o usuário selecionou uma imagem
+    if (this.formGroup.get('avatar').value != '') {
+      const formData = new FormData();
+      formData.append('avatar', this.formGroup.get('avatar').value);
+
+      this.apiService.uploadFile(formData).subscribe(
+        (res) => {
+          this.foto_erro_chamado = res.nomeArquivo;
+
+          let bodyRequest = {
+            requisicao: 'add',
+            nome_usuario: this.formGroup.value.nomeUsuario,
+            email_usuario: this.formGroup.value.emailUsuario,
+            id_nivel_usuario: this.formGroup.value.nivel,
+            login_usuario: this.formGroup.value.loginUsuario,
+            senha: this.formGroup.value.senhaUsuario,
+            foto_erro_chamado: this.foto_erro_chamado,
+          };
+
+          console.log(bodyRequest);
+
+          this.apiService
+            .apiPHP('controller-usuarios.php', bodyRequest)
+            .subscribe((data) => {
+              console.log(bodyRequest);
+
+              if (data['success'] === true) {
+                console.log('Usuário cadastrado');
+              } else {
+                console.log('Erro ao cadastrar usuário');
+              }
+            });
+        },
+        (err) => {
+          console.log('Erro ao cadastrar a imagem ' + err);
+        }
+      );
+    } else {
+      //Caso ele não tenha enviado uma imagem
+      let bodyRequest = {
+        requisicao: 'add',
+        nome_usuario: this.formGroup.value.nomeUsuario,
+        email_usuario: this.formGroup.value.emailUsuario,
+        id_nivel_usuario: this.formGroup.value.nivel,
+        login_usuario: this.formGroup.value.loginUsuario,
+        senha: this.formGroup.value.senhaUsuario,
+        foto_usuario: '',
+      };
+
+      console.log(bodyRequest);
+    }
+
+
     // todo do something with our data like:
     // this.service.set(formData);
   }
+
 
 }
--- src/app/addusuario/addusuario.page.ts
+++ src/app/addusuario/addusuario.page.ts
@@ -12,62 +12,58 @@ export class AddusuarioPage implements OnInit {
   formGroup: FormGroup;
   filePath: string;
   foto_usuario: string;
+  isSubmitted = false;
 
   constructor(
     private apiService: ApiService,
-    
-    private formbuilder: FormBuilder 
-    ) {
-      this.formGroup = formbuilder.group({
-        avatar:[''],
-        nomeUsuario: [
-          "",
-          Validators.compose([
-            Validators.minLength(4),
-            Validators.maxLength(30),
-            Validators.pattern("[0-9a-z-A-Z-_]*"),
-            Validators.required
-          ])
-        ],
-        emailUsuario: [
-          "",
-          Validators.compose([
-            Validators.minLength(4),
-            Validators.maxLength(32),
-            Validators.email,
-            Validators.required
-          ])
-        ],
-        loginUsuario: [
-          "",
-          Validators.compose([
-            Validators.minLength(4),
-            Validators.maxLength(30),
-            Validators.pattern("[0-9a-z-A-Z-_]*"),
-            Validators.required
-          ])
-        ],
-        senhaUsuario: [
-          "",
-          Validators.compose([
-            Validators.minLength(3),
-            Validators.maxLength(255),
-            Validators.required
-          ])
-        ],
-        nivel: [
-          "",
-          Validators.required
-        ],
-        img:[null],
-        filename: ['']
 
-      })
-    }
+    private formbuilder: FormBuilder
+  ) {
+    this.formGroup = formbuilder.group({
+      avatar: [''],
+      nomeUsuario: [
+        '',
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.maxLength(30),
+          Validators.pattern('[0-9a-z-A-Z-_]*'),
+          Validators.required,
+        ]),
+      ],
+      emailUsuario: [
+        '',
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.maxLength(32),
+          Validators.email,
+          Validators.required,
+        ]),
+      ],
+      loginUsuario: [
+        '',
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.maxLength(30),
+          Validators.pattern('[0-9a-z-A-Z-_]*'),
+          Validators.required,
+        ]),
+      ],
+      senhaUsuario: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(255),
+          Validators.required,
+        ]),
+      ],
+      nivel: ['', Validators.required],
+      img: [null],
+      filename: [''],
+    });
+  }
 
   ngOnInit() {
     //Preciso fazer uma requisicao findAll para pegar todos os niveis
-   
   }
   onFileSelect(event) {
     //setando a imagem no formulário para visualização
@@ -83,16 +79,16 @@ export class AddusuarioPage implements OnInit {
     const file = (e.target as HTMLInputElement).files[0];
 
     this.formGroup.patchValue({
-      img: file
+      img: file,
     });
 
-    this.formGroup.get('img').updateValueAndValidity()
+    this.formGroup.get('img').updateValueAndValidity();
 
     const reader = new FileReader();
     reader.onload = () => {
       this.filePath = reader.result as string;
-    }
-    reader.readAsDataURL(file)
+    };
+    reader.readAsDataURL(file);
   }
 
   ionViewWillEnter() {
@@ -113,102 +109,83 @@ export class AddusuarioPage implements OnInit {
         nome: '',
       };
 
-      this.apiService.apiPHP('controller-niveis.php',bodyRequest).subscribe((data) => {
-        if (data['result'] == '0') {
-          this.ionViewWillEnter();
-        } else {
-          data['result'].map((nivel) => {
-            this.niveis.push(nivel[0]);
-          });
-        }
-      });
+      this.apiService
+        .apiPHP('controller-niveis.php', bodyRequest)
+        .subscribe((data) => {
+          if (data['result'] == '0') {
+            this.ionViewWillEnter();
+          } else {
+            data['result'].map((nivel) => {
+              this.niveis.push(nivel[0]);
+            });
+          }
+        });
     });
   }
 
+  get errorControl() {
+    return this.formGroup.controls;
+  }
 
-  ////LEMBRAR PARA SETAR A VARIAVEL ""AVATAR"" COM A RESPOSTA DO JSON do upload para fazer a segunda requisicao
-  
-  // onSubmit(){
-
-  //   //primeira requisicao para fazer o upload da imagem
-  //   return new Promise((res) => {
-  //     this.foto_usuario = '';
-
-  //     const formData = new FormData();
-  //     formData.append('avatar', this.formGroup.get('avatar').value);
-      
-  //     this.apiService.uploadFile(formData).subscribe((data)=> {
-  //       console.log(data);
-  //       this.foto_usuario = data.nomeArquivo;
-  //     })
-  //   });
-
-  //   //segunda requisicao para fazer o cadastro do usuario
+  onSubmit() {
+    this.isSubmitted = true;
     
-   
-  // }
+    if(!this.formGroup.valid){
+      console.log('Por favor preencha todos os campos');
+      return false;
 
-
-  onSubmit(){
+    }
 
     //verificando se o usuário selecionou uma imagem
-      if(this.formGroup.get('avatar').value != ''){
-          const formData = new FormData();
-          formData.append('avatar', this.formGroup.get('avatar').value);
+    if (this.formGroup.get('avatar').value != '') {
+      const formData = new FormData();
+      formData.append('avatar', this.formGroup.get('avatar').value);
 
-          this.apiService.uploadFile(formData).subscribe(
-            (res) => {
-              this.foto_usuario = res.nomeArquivo;
+      this.apiService.uploadFile(formData).subscribe(
+        (res) => {
+          this.foto_usuario = res.nomeArquivo;
 
-              let bodyRequest = {
-                requisicao: 'add',
-                nome_usuario: this.formGroup.value.nomeUsuario,
-                email_usuario: this.formGroup.value.emailUsuario,
-                id_nivel_usuario: this.formGroup.value.nivel,
-                login_usuario: this.formGroup.value.loginUsuario,
-                senha: this.formGroup.value.senhaUsuario,
-                foto_usuario: this.foto_usuario   
-              }
+          let bodyRequest = {
+            requisicao: 'add',
+            nome_usuario: this.formGroup.value.nomeUsuario,
+            email_usuario: this.formGroup.value.emailUsuario,
+            id_nivel_usuario: this.formGroup.value.nivel,
+            login_usuario: this.formGroup.value.loginUsuario,
+            senha: this.formGroup.value.senhaUsuario,
+            foto_usuario: this.foto_usuario,
+          };
 
+          console.log(bodyRequest);
+
+          this.apiService
+            .apiPHP('controller-usuarios.php', bodyRequest)
+            .subscribe((data) => {
               console.log(bodyRequest);
 
-              this.apiService.apiPHP('controller-usuarios.php', bodyRequest).subscribe((data) => {
-                console.log(bodyRequest);
-
-                if (data['success'] === true) {
-                  console.log('Usuário cadastrado');
-                } else {
-                  console.log('Erro ao cadastrar usuário');
-                }
-              }) 
-            },
-            (err) => {  
-              console.log("Erro ao cadastrar a imagem " + err)
-            }
-          );
-      }else{
-        //Caso ele não tenha enviado uma imagem
-        let bodyRequest = {
-          requisicao: 'add',
-          nome_usuario: this.formGroup.value.nomeUsuario,
-          email_usuario: this.formGroup.value.emailUsuario,
-          id_nivel_usuario: this.formGroup.value.nivel,
-          login_usuario: this.formGroup.value.loginUsuario,
-          senha: this.formGroup.value.senhaUsuario,
-          foto_usuario: ''   
+              if (data['success'] === true) {
+                console.log('Usuário cadastrado');
+              } else {
+                console.log('Erro ao cadastrar usuário');
+              }
+            });
+        },
+        (err) => {
+          console.log('Erro ao cadastrar a imagem ' + err);
         }
-    
-        console.log(bodyRequest);
-      }
+      );
+    } else {
+      //Caso ele não tenha enviado uma imagem
+      let bodyRequest = {
+        requisicao: 'add',
+        nome_usuario: this.formGroup.value.nomeUsuario,
+        email_usuario: this.formGroup.value.emailUsuario,
+        id_nivel_usuario: this.formGroup.value.nivel,
+        login_usuario: this.formGroup.value.loginUsuario,
+        senha: this.formGroup.value.senhaUsuario,
+        foto_usuario: '',
+      };
 
+      console.log(bodyRequest);
+    }
   }
-  
-
-
-
-
-  
-
-  
-
 }
