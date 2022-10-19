d55f27a9d826db623f455d6e8b298eb8b3cef004
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-10-19 18:37:00 -0300

    iniciacao cadastro cliente

--- src/app/cadastro-cliente/cadastro-cliente.page.ts
+++ src/app/cadastro-cliente/cadastro-cliente.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { ApiService } from '../api/api-service';
 
 @Component({
   selector: 'app-cadastro-cliente',
@@ -7,7 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroClientePage implements OnInit {
 
-  constructor() { }
+  formGroupPf: FormGroup;
+  isSubmitrted: boolean = false;
+  formGroupPj: FormGroup;
+
+  constructor(
+    private router: Router,
+    private formbuilder: FormBuilder,
+    private apiService: ApiService,
+    private toastController: ToastController
+  ) { 
+    this.formGroupPf = formbuilder.group({
+      nome:[
+        "",
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.maxLength(60),
+          Validators.required
+        ])
+      ],
+      cpf:[
+        "",
+        Validators.compose([
+          Validators.minLength(1),
+          Validators.maxLength(11),
+          Validators.pattern('[0-9]*'),
+          Validators.required
+        ])
+      ],
+      email:[
+        "",
+        Validators.compose([
+          Validators.minLength(6),
+          Validators.maxLength(32),
+          Validators.email,
+          Validators.required
+        ])
+      ],
+      telefone:[
+        "",
+        Validators.compose([
+          Validators.minLength(8),
+          Validators.maxLength(11),
+          Validators.pattern('[0-9]*'),
+          Validators.required
+        ])
+      ],
+      senha:[
+        "",
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.maxLength(255),
+          Validators.required
+        ])
+      ]
+    });
+  }
 
   ngOnInit() {
   }
